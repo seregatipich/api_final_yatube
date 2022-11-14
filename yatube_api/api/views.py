b7061beba8fb02c62f1ac01631258e43a1edd2ce
Sergei Poluektov <poluektovsergei345@gmail.com>
2022-11-14 20:52:53 +0100

    fixed some stuff

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -6,7 +6,7 @@ from rest_framework.pagination import LimitOffsetPagination
 from api.permissions import AuthorOrReading
 from api.serializers import (CommentSerializer, FollowSerializer,
                              GroupSerializer, PostSerializer)
-from posts.models import Follow, Group, Post, User
+from posts.models import Group, Post
 
 
 class PostViewSet(viewsets.ModelViewSet):
